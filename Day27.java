fb4f4b3347e348fdb56b5111bab18964bdf77d03
pranavj1001 <pranavj1001@gmail.com>
2016-09-13 21:59:00 +0530

    added Day27 java solution

---
+++ Day27.java
@@ -0,0 +1,27 @@
+package com.ThirtyDaysOfCode;
+
+public class Day27 {
+
+    public static void main(String[] args) {
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("5");
+        
+        System.out.println("10 7");
+        System.out.println("-1 2 3 5 3 0 3 5 0 0");
+        
+        System.out.println("4 3");
+        System.out.println("-1 0 -1 4");
+        
+        System.out.println("5 3");
+        System.out.println("-1 0 1 2 4");
+        
+        System.out.println("6 3");
+        System.out.println("-1 0 -5 4 5 3");
+        
+        System.out.println("7 3");
+        System.out.println("-1 3 5 4 0 3 4");
+    }
+        
+}
+
